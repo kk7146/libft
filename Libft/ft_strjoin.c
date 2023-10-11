82427f983fdf1ae97953f8033a37f7f90ede949e
Dongeun Son <donson@cx1r5s8.42seoul.kr>
2023-10-11 20:02:32 +0900

    TodayLastCommit

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -1,24 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: donson <donson@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/11 16:40:04 by donson            #+#    #+#             */
-/*   Updated: 2023/10/11 19:55:55 by donson           ###   ########.fr       */
+/*   Created: 2023/10/11 19:14:43 by donson            #+#    #+#             */
+/*   Updated: 2023/10/11 20:02:06 by donson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	void	*p;
+	char	*result;
+	size_t	len_s1;
+	size_t	len_s2;
 
-	p = ((void *)malloc((count * size)));
-	if (!p)
+	if (s1 == NULL || s2 == NULL)
+		return ((void *)calloc(1, 1));
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	result = (char *)calloc(len_s1 + len_s2 + 1, sizeof(char));
+	if (!result)
 		return (NULL);
-	ft_bzero(p, size * count);
-	return (p);
+	ft_memmove(result, s1, len_s1);
+	ft_memmove(result + len_s1, s2, len_s2);
+	result[len_s1 + len_s2] = '\0';
+	return (result);
 }
--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,52 @@
 /*   By: donson <donson@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:58:57 by donson            #+#    #+#             */
-/*   Updated: 2023/10/11 18:00:03 by donson           ###   ########.fr       */
+/*   Updated: 2023/10/11 19:14:07 by donson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_substr_dynamic(char const *s, unsigned int start, size_t len)
+{
+	char	*result;
+
+	result = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!result)
+	{
+		free(result);
+		return (NULL);
+	}
+	ft_memmove(result, s + start, len);
+	result[len] = '\0';
+	return (result);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	s_len;
+	char	*result;
+
+	s_len = ft_strlen(s);
+	if (start + len <= s_len)
+	{
+		result = ft_substr_dynamic(s, start, len);
+		return (result);
+	}
+	else if (start < s_len && len <= s_len)
+	{
+		len -= start + len - s_len;
+		result = ft_substr_dynamic(s, start, len);
+		return (result);
+	}
+	else
+	{
+		result = (char *)ft_calloc(1, 1);
+		if (!result)
+		{
+			free(result);
+			return (NULL);
+		}
+		return (result);
+	}
+}
