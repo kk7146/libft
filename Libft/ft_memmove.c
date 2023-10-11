3fbcb11c04a7996c17b58319cabb51e02e63e63a
kk7146 <kk21322132@icloud.com>
2023-10-11 11:51:27 +0900

    FirstCommit

---
+++ Libft/ft_memmove.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eun <eun@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 17:57:07 by donson            #+#    #+#             */
+/*   Updated: 2023/10/11 11:05:13 by eun              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	if (len == 0)
+		return (dst);
+	s = (unsigned char *)src;
+	d = (unsigned char *)dst;
+	if (dst <= src)
+		while (len--)
+			*d++ = *s++;
+	else
+	{
+		d += len - 1;
+		s += len - 1;
+		while (len--)
+			*d-- = *s--;
+	}
+	return (dst);
+}
